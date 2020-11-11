89c13a1396f18dd34be02ce6a15e8d07c56e776d
mofath <mohamed.elgaedi@gmail.com>
2020-11-11 19:35:14 +0200

    Revert "Added DBManager sigelton class"
    This reverts commit 9b3f41e99afc1ac7c8d51bedb3ad0742ff223616.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,9 @@
 const http = require("http");
 const app = require("./app");
 const config = require("./config");
-const DBManager = require("./lib/DBManager");
 
 const server = http.createServer(app);
 
 const PORT = config.PORT;
 
-const db = DBManager.getInstance();
-db.conn.once("open", (err) => {
-  if (err) console.log("Database connection failure");
-  else {
-    console.log("Database opened");
-    server.listen(PORT, () =>
-      console.log(`server is listening at port ${PORT}`)
-    );
-  }
-});
+server.listen(PORT, () => console.log(`server is listening at port ${PORT}`));
