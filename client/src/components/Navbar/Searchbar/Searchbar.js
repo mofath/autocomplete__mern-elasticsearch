d19f13ccb7b276729c0f69a238b51df284d29820
mofath <mohamed.elgaedi@gmail.com>
2020-11-12 14:41:56 +0200

    Adjust searchbar styling

--- client/src/components/Navbar/Searchbar/Searchbar.js
+++ client/src/components/Navbar/Searchbar/Searchbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FiSearch as SearchIcon } from "react-icons/fi";
+import { VscChromeClose as CloseIcon } from "react-icons/vsc";
 
 import classes from "./Searchbar.module.css";
 
@@ -9,24 +10,34 @@ const Searchbar = () => {
 
   return (
     <div
-      className={[classes.Search, SearchActive && classes.SearchActive].join(" ")}
+      className={[
+        classes.Searchbar,
+        SearchActive && classes.ActiveSearchbar,
+      ].join(" ")}
     >
       <input
         className={[
           classes.SearchInput,
-          SearchActive && classes.ActiveSearcInput,
+          SearchActive && classes.ActiveSearchInput,
         ].join(" ")}
-        type="search"
+        type="text"
         value={SearchTerm}
         onChange={({ target }) => setSearchTerm(target.value)}
         placeholder="Search"
+        onMouseEnter={() => setSearchActive(true)}
+        onMouseLeave={() => setSearchActive(false)}
+        onFocus={() => setSearchActive(true)}
       />
       <button
         className={classes.SearchIcon}
         onClick={() => setSearchActive((SearchActive) => !SearchActive)}
       >
         <i>
-          <SearchIcon color="black" size="30px" />
+          {SearchActive ? (
+            <CloseIcon color="black" size="30px" />
+          ) : (
+            <SearchIcon color="black" size="30px" />
+          )}
         </i>
       </button>
     </div>
