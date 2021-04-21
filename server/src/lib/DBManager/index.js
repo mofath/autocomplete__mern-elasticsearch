67b2988ee569e73b0dae34445ca84c864586098f
mofath <mohamed.elgaedi@gmail.com>
2021-04-21 04:19:28 +0200

    [server]: Updated DBManager module and added disconect method

--- server/src/lib/DBManager/index.js
+++ server/src/lib/DBManager/index.js
@@ -4,38 +4,33 @@ const config = require("../../config");
 const DB_URL = config.mongoose.url;
 const options = config.mongoose.options;
 
-
-let instance = null;
 class DBManager {
   constructor() {
-    this._conn = null;
-  }
-
-  static getInstance() {
-    if (!instance) {
-      instance = new DBManager();
+    if (!DBManager.instance) {
+      this._conn = mongoose;
+      DBManager.instance = this;
     }
-    return instance;
+    return DBManager.instance;
   }
 
   CONNECT() {
     console.log("Connecting to db...");
-    mongoose.connect(DB_URL, options);
-    this._conn = mongoose.connection;
+    this._conn.connect(DB_URL, options);
+    return this._conn.connection;
+  }
+
+  DISCONNECT(){
+    console.log("Closing db connection...");
+    this._conn.connection.close();
   }
 
   get conn() {
     this.CONNECT();
-    return this._conn;
+    return this._conn.connection;
   }
 }
 
-mongoose.connection.on("error", (err) => {
-  console.log("Mongoose connected error " + err);
-});
+const instance = new DBManager()
+Object.freeze(instance)
 
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose disconnected");
-});
-
-module.exports = DBManager;
+module.exports = instance;
