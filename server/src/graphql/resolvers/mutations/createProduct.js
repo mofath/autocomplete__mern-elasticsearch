bb0f96b8fdb96f8ef3dd7239cc108aa8b3805af2
mofath <mohamed.elgaedi@gmail.com>
2021-04-22 02:49:21 +0200

    [server]: updated createProduct service

--- server/src/graphql/resolvers/mutations/createProduct.js
+++ server/src/graphql/resolvers/mutations/createProduct.js
@@ -1,27 +1,19 @@
 const productService = require("../../../services");
-const { addDocument } = require("../../../lib/elasticsearch");
+const esService = require("../../../lib/elasticsearch");
 const { PRODUCTS_INDEX, PRODUCTS_TYPE } = require("../../../constant/elasticsearch-constants");
 const mappings = require("../../../constant/mappings.json");
 
-exports.createProduct = async (obj, { productInput }, { esClient }, info) => {
+exports.createProduct = async (obj, { productInput }, ctx, info) => {
   try {
-    const createdProduct = await productService.createProduct(productInput)
-
-    const document = {
-      name: createdProduct.name,
-      image: createdProduct.image,
-      brand: createdProduct.brand,
-      id: createdProduct.id,
-      suggest: { input: createdProduct.name.split(" ") }
-    }
-
-    await addDocument(PRODUCTS_INDEX, PRODUCTS_TYPE, document, mappings.productsMapping);
-
+    const { id } = await productService.createProduct(productInput);
+    const { price, ...indexingData } = productInput;
+    const document = { id, ...indexingData, suggest: { input: productInput.name.split(" ") } }
+    await esService.addDocument(PRODUCTS_INDEX, PRODUCTS_TYPE, document, mappings.productsMapping);
     delete document.suggest;
-    document.price = createdProduct.price;
+    document.price = price;
     return document;
   } catch (error) {
-    console.log(error.message);
+    console.log("Something went wrong: GraphQL mutatuion: createProduct:", error.message);
     throw error;
   }
 }
--- server/src/services/createProduct.js
+++ server/src/services/createProduct.js
@@ -2,16 +2,11 @@ const { ProductModel } = require("../models/product.model");
 
 exports.createProduct = async (serviceData) => {
     try {
-        const product = new ProductModel({
-            name: serviceData.name,
-            price: serviceData.price,
-            brand: serviceData.brand,
-            image: serviceData.image,
-        });
+        const product = new ProductModel({ ...serviceData });
         const result = await product.save();
         return result;
     } catch (error) {
-        console.log("Something went wrong: Service: createProduct", error);
+        console.log("Something went wrong: Service: createProduct", error.message);
         throw new Error(error);
     }
 };
